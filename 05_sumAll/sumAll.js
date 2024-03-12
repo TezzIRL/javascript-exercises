723faa71dc47a7afd6317a987ec7a706036be090
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-12 20:18:05 +1100

    Add Solution to Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR'
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
 
+    let largestNumber = num1 > num2 ? num1 : num2
+    let smallestNumber = num1 < num2 ? num1 : num2
+    let answer = 0;
+    while (smallestNumber <= largestNumber) {
+        answer += smallestNumber
+        smallestNumber++
+    }
+    return answer
 };
 
 // Do not edit below this line
