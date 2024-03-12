ce6c63cf811e4f90111e1ae071f5a1a7b8d21bfb
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-12 20:37:04 +1100

    Add Solution to Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let cel = ((fahrenheit - 32) * 5 / 9).toFixed(1)
+  return parseFloat(cel);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fah = ((celsius * (9 / 5)) + 32).toFixed(1)
+  return parseFloat(fah)
 };
 
 // Do not edit below this line
