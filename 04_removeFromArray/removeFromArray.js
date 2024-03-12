849c786768a230eaa6767b9d88670323f398305b
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-12 20:00:06 +1100

    Add Solution to Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+    let i = 0;
+    while (i < arr.length) {
+        if (args.includes(arr[i])) {
+            arr.splice(i, 1)
+        } else {
+            i++
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
