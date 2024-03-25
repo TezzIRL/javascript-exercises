49de85e64064606911609f0195c6ad2092cd2b35
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-25 13:23:27 +1100

    Add Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let sequence = [0, 1];
 
+    const nth = parseInt(n)
+    if (nth < 0) return "OOPS"
+    if (nth === 0) return 0;
+
+    for (let index = 2; index <= nth; index++) {
+        let value = sequence[index - 1] + sequence[index - 2]
+        sequence.push(value)
+    }
+    return sequence.pop()
 };
 
 // Do not edit below this line
