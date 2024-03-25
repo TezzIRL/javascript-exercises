30f61ec9b1c6748b79c3e2e842df706d073db967
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-25 12:17:50 +1100

    Add Solution to Palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // regex filter
+    const alpha_num = /[a-zA-Z0-9]/
+    const cleaned_string = string.toLowerCase()
+        .split("")
+        .filter((char) => char.match(alpha_num))
+        .join()
 
+    const reversed = cleaned_string.split("")
+        .reverse()
+        .join("")
+
+    return cleaned_string === reversed
 };
 
 // Do not edit below this line
