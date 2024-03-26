2a2a7fa0bd46a7b854b9bddeeed17b25df897963
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-26 12:14:09 +1100

    Add Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    // calculate age by death year or current year - birth year
+    // return highest age
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
 
+        return oldestAge > currentAge ? oldest : current
+    })
 };
+
+const getAge = (birthYear, deathYear) => {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear()
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
